abb2d5dd4b5635ea55cdd16f1b8bd781c0106da6
carolinasntos <a01174992@tec.mx>
2025-04-01 18:04:16 -0600

    D

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// CAMBIA esto por el nombre exacto de tu repositorio de GitHub
 export default defineConfig({
+  base: '/mi-proyecto-react/', 
   plugins: [react()],
 })
